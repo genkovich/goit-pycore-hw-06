b7d2b8891d20b589f122e9948c58e5f86a5cee5a
genkovich <genkovich@syneforge.com>
2024-05-11 22:03:57 +0200

    :boom: initial commit

---
+++ address_book.py
@@ -0,0 +1,63 @@
+from collections import UserDict
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Name(Field):
+    def __init__(self, value):
+        super().__init__(value)
+
+
+class Phone(Field):
+    def __init__(self, value):
+        if (len(value) < 10) or (len(value) > 15):
+            raise ValueError("Phone number must be between 10 and 15 digits")
+        super().__init__(value)
+
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def edit_phone(self, old_phone, new_phone):
+        for phone in self.phones:
+            if phone.value == old_phone:
+                phone.value = new_phone
+                break
+
+    def find_phone(self, phone):
+        result = None
+        for phone_value in self.phones:
+            if phone_value.value == phone:
+                result = phone_value
+                break
+
+        return result
+
+    def remove_phone(self, phone):
+        phone_value = self.find_phone(phone)
+        self.phones.remove(phone_value)
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data[name]
+
+    def delete(self, name):
+        del self.data[name]
